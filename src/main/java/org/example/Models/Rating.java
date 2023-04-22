28414cb9597fba860a163a2d0fe4c9fec34d8e3e
Micha_Serega <VolkovML@mpei.ru>
2023-04-22 17:20:01 +0300

    thoo

--- src/main/java/org/example/Models/Rating.java
+++ src/main/java/org/example/Models/Rating.java
@@ -1,2 +1,24 @@
-package org.example.Models;public class Rating {
+package org.example.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Rating")
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "rating")
+    private int rating;
+
+    @ManyToMany(mappedBy = "rating")
+    private List<Student> student;
 }
